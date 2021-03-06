265c189e2f0d1f72a818d0661fe68010f9598af2
Jonathan Mun <jguy25@Jonathans-MacBook-Pro.local>
2021-03-06 16:02:34 -0500

    fixed

--- linear_regression.py
+++ linear_regression.py
@@ -3,23 +3,21 @@ np.random.seed(1)
 
 class lin_reg():
     
-    def __init__(self,trainDataX,trainDataY,hasIntercept=False):
-        self.hasIntercept = hasIntercept
-        if hasIntercept:
-            self.theta = np.random.rand(1,input.shape[1]+1)
-        else:
-            self.theta = np.random.rand(1,input.shape[1])
+    def __init__(self,trainDataX,trainDataY):
+        self.theta = np.random.rand(1,input.shape[1])
         self.trainDataX = trainDataX
         self.trainDataY = trainDataY
 
-
-    def predict(self,X):
-        if self.hasIntercept:            
-            y = np.dot(X,self.theta[:,1:].T) + self.theta[:,0]
-        else:
-            y = np.dot(X,self.theta.T)
+    def predict(self,X,theta):
+        y = np.dot(X,theta.T)
         return y
     
+    def cost(self,x,theta):
+        y = self.trainDataY
+        yhat = self.predict(x,theta)
+        m = y.shape[1]
+        XO_y = yhat-y
+        return 1/2/m*np.dot(XO_y.T,XO_y)
 
     def gradientDesc(self,alpha,epochs):
         theta = self.theta
@@ -27,27 +25,27 @@ class lin_reg():
         y = self.trainDataY
         m = self.trainDataY.shape[0]
         for i in range(epochs):
-            yhat = self.predict(self.trainDataX)
-            for t in range(theta.shape[1]):
-                d_dt = np.add(-yhat,y)
-                if self.hasIntercept:
-                    if t == 0:
-                        theta[:,t] = theta[:,t] - alpha/m*np.sum(d_dt)
-                    else:
-                        #ex. x has 2 col, theta has 3. thus column relation is x=theta-1 
-                        xcol = t-1
-                        theta[:,t] = theta[:,t] - alpha/m*np.sum(np.dot(d_dt.T,x))
-                else:
-                    theta[:,t] = theta[:,t] - alpha/m*np.dot(d_dt.T,x)
+            yhat = self.predict(x,theta)
+            
+            d_dt = np.add(yhat,-y) #nx1
+            theta = theta - alpha/m*np.dot(d_dt.T,x)#2xn nx1
+            cost = self.cost(x,theta)
+            #print(cost)
+
         return theta
+    
+    def closedSolution(self):
+        x = self.trainDataX
+        y = self.trainDataY
+
+        return np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
 
 
 
 if __name__ == '__main__':
-    input = np.random.rand(100,1)
-    output = np.zeros((100,1))
-    l = lin_reg(input,output,False)
-    y = l.predict(input)
-    print(y)
-    g = l.gradientDesc(.05,1000)
+    input = np.array([[1,2,3,4,5,6,7,8,9],[1,1,1,1,1,1,1,1,1]]).T
+    output = np.array([[2,4,6,8,10,12,14,16,18]]).T
+    l = lin_reg(input,output)
+    g = l.gradientDesc(.05,10000)
     print(g)
+    print(l.closedSolution())
