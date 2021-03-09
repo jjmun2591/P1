d5bfcfe6c88843b1767a382d07b4f165022c3607
Jonathan Mun <jguy25@Jonathans-MacBook-Pro.local>
2021-03-09 10:11:38 -0500

    duh

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from n_graph import *
 np.random.seed(1)
 
 class lin_reg():
@@ -7,7 +8,7 @@ class lin_reg():
         self.theta = np.random.rand(1,input.shape[1])
         self.trainDataX = trainDataX
         self.trainDataY = trainDataY
-
+        
     def predict(self,X,theta):
         y = np.dot(X,theta.T)
         return y
@@ -16,36 +17,51 @@ class lin_reg():
         y = self.trainDataY
         yhat = self.predict(x,theta)
         m = y.shape[1]
-        XO_y = yhat-y
-        return 1/2/m*np.dot(XO_y.T,XO_y)
+        delta = yhat-y
+        return 1/2/m*np.dot(delta.T,delta)
 
-    def gradientDesc(self,alpha,epochs):
+    def gradientDesc(self,alpha,epochs,testing=False):
         theta = self.theta
         x = self.trainDataX
         y = self.trainDataY
         m = self.trainDataY.shape[0]
+
+        
+        theta_cost = np.zeros((epochs,x.shape[1]+1))
         for i in range(epochs):
             yhat = self.predict(x,theta)
             
-            d_dt = np.add(yhat,-y) #nx1
-            theta = theta - alpha/m*np.dot(d_dt.T,x)#2xn nx1
-            cost = self.cost(x,theta)
-            #print(cost)
+            delta = np.add(yhat,-y) #nx1
+            ddJ = np.dot(delta.T,x) #2xn nx1
+            theta = theta - alpha/m*ddJ
 
+            if testing:
+
+                for j in range(x.shape[1]+1):
+                    if j==x.shape[1]:
+                        theta_cost[i,j] = self.cost(x,theta)
+                    else:
+                        theta_cost[i,j] = theta[:,j]
+        if testing:
+            ng = n_graph(theta_cost[:,0].reshape((epochs,1)),
+            theta_cost[:,1].reshape((epochs,1)),
+            theta_cost[:,2].reshape((epochs,1)),
+            np.arange(0,epochs))
+            ng.plot()
         return theta
     
     def closedSolution(self):
         x = self.trainDataX
         y = self.trainDataY
-
+        # theta = (X.T*X)^-1*X.T*y
         return np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y))
 
 
 
 if __name__ == '__main__':
-    input = np.array([[1,2,3,4,5,6,7,8,9],[1,1,1,1,1,1,1,1,1]]).T
-    output = np.array([[2,4,6,8,10,12,14,16,18]]).T
+    input = np.random.rand(100,10)
+    output = np.random.rand(100,1)
     l = lin_reg(input,output)
-    g = l.gradientDesc(.05,10000)
+    g = l.gradientDesc(.0005,1000,True)
     print(g)
     print(l.closedSolution())
